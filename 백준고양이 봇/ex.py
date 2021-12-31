1392f6273a9a5996ee5ae12b1fc83c47f499a40d
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-01-01 00:34:51 +0900

    진행 중

--- 백준고양이 봇/ex.py
+++ 백준고양이 봇/ex.py
@@ -2,35 +2,44 @@
 from discord.ext import commands
 import discord
 from bs4 import BeautifulSoup
+import json
 
-class Example(commands.Cog):
+col =  "파일위치"
+class Bot(commands.Cog):
     def __init__(self, client):
         self.client = client
+        #데이터 파일 불러오기
+        with open(col,"r", encoding="utf-8") as f:
+            self.quizDict = json.load(f)
     
     @commands.Cog.listener()
     async def on_ready(self):
         print("example Cog is Ready")
         
-    @commands.command(name = "ping")
+    @commands.command(name = "병윤아")
     async def _ping(self, ctx):
-        await ctx.send('pong!')
+        await ctx.send('화이팅!!!')
 
 
     @commands.command(name = "백준")
-    async def quiz(ctx, self,data, quiz):
-
-        registrant = ctx.author.name
-        def checkMessage(message):
-            return message.author == ctx.author and message.channel == ctx.channel
+    async def quiz(self, ctx):
+        embed = discord.Embed(title ="★고양이도우미★", description="아래기능을 사용할 수 있습니다.", color=discord.Color.blue())
         
-        
-        embed = discord.Embed(tittle='문제 검색', description= '풀이 기간, 문제 번호 또는 이름을 입력 해주세요 ', color = discord.Color.blue())
+        embed.add_field(name="1.문제등록", value="'!문제등록' 키워드를 사용하시면 됩니다.", inline=False)
+        embed.add_field(name="2.이번주문제", value="'!이번주문제' 키워드를 사용하시면 됩니다.", inline=False)
+        embed.add_field(name="3.지난주문제", value="'!지난주문제' 키워드를 사용하시면 됩니다.", inline=False)
         await ctx.send(embed = embed)
 
-        message = await self.client.wait_for("message", check = checkMessage)
+
+    @commands.command(name = "문제등록")
+    async def quizreg(self, ctx):
+        def checkMessage(message):
+                return message.author == ctx.author and message.channel == ctx.channel
         
-        await message.send("f{}기간 동안 {}문제를 풀어보아요", data, quiz)
-  
+        embed = discord.Embed(title = '문제등록', description = '마감 기한-문제번호-문제명 순으로 입력해주세요.', color=discord.Color.blue())
+        embed.add_field(name="예시", value="'20220101\0 1237\0 정ㅋ벅ㅋ", inline=False)
+        await ctx.send(embed = embed)
+
 
 def setup(client):
-    client.add_cog(Example(client))
+    client.add_cog(Bot(client))
