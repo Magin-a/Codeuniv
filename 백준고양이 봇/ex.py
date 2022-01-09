ee0372f909ee0ac5ff93e14fe72b5693ce287d80
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-01-10 00:05:38 +0900

    Update ex.py

--- 백준고양이 봇/ex.py
+++ 백준고양이 봇/ex.py
@@ -16,11 +16,7 @@ class Bot(commands.Cog):
     async def on_ready(self):
         print("example Cog is Ready")
         
-    @commands.command(name = "병윤아")
-    async def _ping(self, ctx):
-        await ctx.send('화이팅!!!')
-
-
+    
     @commands.command(name = "백준")
     async def quiz(self, ctx):
         embed = discord.Embed(title ="★고양이도우미★", description="아래기능을 사용할 수 있습니다.", color=discord.Color.blue())
@@ -33,12 +29,20 @@ class Bot(commands.Cog):
 
     @commands.command(name = "문제등록")
     async def quizreg(self, ctx):
-        def checkMessage(message):
-                return message.author == ctx.author and message.channel == ctx.channel
+        def checkMessage(message, user):
+                return user == message.author 
         
         embed = discord.Embed(title = '문제등록', description = '마감 기한-문제번호-문제명 순으로 입력해주세요.', color=discord.Color.blue())
-        embed.add_field(name="예시", value="'20220101\0 1237\0 정ㅋ벅ㅋ", inline=False)
+        embed.add_field(name="예시", value="'20220101\0 1237\0 정ㅋ벅ㅋ'", inline=False)
+        embed.add_field(name="다음 입력: 문제 수", value="이번주의 문제는 몇문제 인가요?")
+        # embed.add_field(name="다음 명령어", value="위 예시처럼 입력을 해주세요. 주의사항) 입력 순서!!", inline=False)
         await ctx.send(embed = embed)
+
+        quiz_num = await self.client.wait_for("message", check = checkMessage)
+        for _ in range(int(quiz_num)):
+            
+
+        await ctx.send()
 
 
 def setup(client):
